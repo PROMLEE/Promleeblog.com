6f81ec3f15c2f7ff338d62a7b16d71e5ac187867
PROMLEE <donghoon099@naver.com>
2024-04-22 00:51:11 +0900

    [Feat] add mdx editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,61 @@
-export default async function Home() {
+"use client";
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  toolbarPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  CodeToggle,
+  BlockTypeSelect,
+  CreateLink,
+  linkDialogPlugin,
+  diffSourcePlugin,
+  InsertFrontmatter,
+  frontmatterPlugin,
+  imagePlugin,
+  InsertImage,
+  tablePlugin,
+  InsertTable,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
+
+export default function App() {
   return (
-    <div>
-      <div className={"m-5 text-3xl"}>Hello, I am Promlee</div>
+    <div className={"prose"}>
+      <MDXEditor
+        markdown={"# Hello World"}
+        plugins={[
+          headingsPlugin(),
+          listsPlugin(),
+          quotePlugin(),
+          thematicBreakPlugin(),
+          markdownShortcutPlugin(),
+          linkDialogPlugin(),
+          diffSourcePlugin(),
+          frontmatterPlugin(),
+          imagePlugin(),
+          tablePlugin(),
+          toolbarPlugin({
+            toolbarContents: () => (
+              <>
+                {" "}
+                <UndoRedo />
+                <BoldItalicUnderlineToggles />
+                <BlockTypeSelect />
+                <CodeToggle />
+                <CreateLink />
+                <InsertFrontmatter />
+                <InsertImage />
+                <InsertTable />
+              </>
+            ),
+          }),
+        ]}
+      />
     </div>
   );
 }
