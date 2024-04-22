9992109594c9c5ac34a3ed74ea7be7d6fd0f0b7a
PROMLEE <donghoon099@naver.com>
2024-04-22 19:45:15 +0900

    [feat] ready for post

---
+++ src/lib/mdxeditor.tsx
@@ -0,0 +1,59 @@
+import {
+  headingsPlugin,
+  listsPlugin,
+  quotePlugin,
+  thematicBreakPlugin,
+  markdownShortcutPlugin,
+  MDXEditor,
+  toolbarPlugin,
+  UndoRedo,
+  BoldItalicUnderlineToggles,
+  CodeToggle,
+  BlockTypeSelect,
+  CreateLink,
+  linkDialogPlugin,
+  diffSourcePlugin,
+  InsertFrontmatter,
+  frontmatterPlugin,
+  imagePlugin,
+  InsertImage,
+  tablePlugin,
+  InsertTable,
+} from "@mdxeditor/editor";
+import "@mdxeditor/editor/style.css";
+
+export default function App() {
+  return (
+    <div className={"prose"}>
+      <MDXEditor
+        markdown={""}
+        plugins={[
+          headingsPlugin(),
+          listsPlugin(),
+          quotePlugin(),
+          thematicBreakPlugin(),
+          markdownShortcutPlugin(),
+          linkDialogPlugin(),
+          diffSourcePlugin(),
+          frontmatterPlugin(),
+          imagePlugin(),
+          tablePlugin(),
+          toolbarPlugin({
+            toolbarContents: () => (
+              <>
+                <UndoRedo />
+                <BoldItalicUnderlineToggles />
+                <BlockTypeSelect />
+                <CodeToggle />
+                <CreateLink />
+                <InsertFrontmatter />
+                <InsertImage />
+                <InsertTable />
+              </>
+            ),
+          }),
+        ]}
+      />
+    </div>
+  );
+}
