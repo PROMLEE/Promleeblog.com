6bdc0c8290f3d70e20cbddb5b7deec43b2177fc1
PROMLEE <donghoon099@naver.com>
2024-06-11 21:06:39 +0900

    chore: Export CategoryKoinfo interface in koname.ts

---
+++ src/app/sitemap.ts
@@ -0,0 +1,59 @@
+import { MetadataRoute } from "next";
+import { CategoryKo } from "@/config/koname";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const basePath = "https://promleeblog.com/blog/";
+
+  const putmap: any = (url: string) => {
+    return {
+      url: basePath + url,
+      lastModified: new Date(),
+      // changeFrequency: "daily",
+      // priority: 0.8,
+    };
+  };
+  var list: any[] = [
+    {
+      url: "https://promleeblog.com",
+      lastModified: new Date(),
+      // changeFrequency: 'daily',
+      // priority: 0.8,
+      // alternates: {
+      //   languages: {
+      //     es: 'https://witch.work/es',
+      //     en: 'https://witch.work/en',
+      //   }
+      // }
+    },
+    {
+      url: "https://promleeblog.com/aboutme",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://promleeblog.com/blog",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://promleeblog.com/sitemap",
+      lastModified: new Date(),
+    },
+  ];
+  Object.keys(CategoryKo).map((category) => {
+    list.push(putmap(category));
+    Object.keys(CategoryKo[category].sub).map((sub) => {
+      console.log(sub);
+      list.push(putmap(category + "/" + sub));
+      Object.keys(CategoryKo[category].sub[sub].title).map((title) => {
+        list.push(putmap(category + "/" + sub + "/" + title));
+        Object.keys(CategoryKo[category].sub[sub].title[title].content).map(
+          (content) => {
+            list.push(
+              putmap(category + "/" + sub + "/" + title + "/" + content),
+            );
+          },
+        );
+      });
+    });
+  });
+  return list;
+}
