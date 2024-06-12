aa72198c77d1aebcc3d7d9c932479eb1c4cfde6c
PROMLEE <donghoon099@naver.com>
2024-06-12 12:17:01 +0900

    chore: Add Scrollindicator component to Navbar and update button styles

--- src/components/buttons/Todown.tsx
+++ src/components/buttons/Todown.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 export const Todown = () => {
+  const { theme, setTheme } = useTheme();
   return (
     <Link href="#bottom">
       <svg
-        className="sticky bottom-0 ml-auto mt-5 h-8 w-8 rounded-t-md bg-foreground"
+        className="sticky bottom-0 z-10 ml-auto mt-5 h-8 w-8 rounded-t-md bg-foreground"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -14,7 +16,7 @@ export const Todown = () => {
         viewBox="0 0 24 24"
       >
         <path
-          stroke="black"
+          stroke={theme === "dark" ? "#202124" : "white"}
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="2"
--- src/components/buttons/Toup.tsx
+++ src/components/buttons/Toup.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import { useTheme } from "next-themes";
 
 export const Toup = () => {
+  const { theme, setTheme } = useTheme();
   return (
     <Link href="#">
       <svg
-        className="sticky top-0 mb-2 ml-auto h-8 w-8 rounded-b-md bg-foreground"
+        className="sticky top-0 z-10 mb-2 ml-auto h-8 w-8 rounded-b-md bg-foreground"
         aria-hidden="true"
         xmlns="http://www.w3.org/2000/svg"
         width="24"
@@ -14,7 +16,7 @@ export const Toup = () => {
         viewBox="0 0 24 24"
       >
         <path
-          stroke="black"
+          stroke={theme === "dark" ? "#202124" : "white"}
           strokeLinecap="round"
           strokeLinejoin="round"
           strokeWidth="2"
