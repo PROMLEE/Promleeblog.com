dc83a3a2a5412b7cab150b3e5b57879a069ecdb5
PROMLEE <donghoon099@naver.com>
2024-06-14 19:18:16 +0900

    feat: Generate metadata for about me and sitemap pages

---
+++ src/app/aboutme/layout.tsx
@@ -0,0 +1,46 @@
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const baseUrl = "https://promleeblog.com";
+  const flattenedPath = "aboutme";
+  const thumbnail = "icons/android-chrome-512x512.png";
+  const title = "About Me | PromleeBlog";
+  const description = "풀스택 개발자 이동훈의 포트폴리오입니다. - PromleeBlog";
+  const tags = ["About Me", "Portfolio", "포트폴리오", "풀스택", "개발자"];
+  return {
+    title,
+    description,
+    keywords: tags,
+    openGraph: {
+      title,
+      description,
+      url: `${baseUrl}/${flattenedPath}`,
+      siteName: "PromleeBlog",
+      images: {
+        url:
+          `${baseUrl}/${thumbnail}` ||
+          `${baseUrl}/icons/android-chrome-512x512.png`,
+        alt: "Post Image",
+      },
+      locale: "ko_KR",
+      type: "article",
+      tags,
+    },
+    twitter: {
+      card: "summary_large_image",
+      creator: "PromleeBlog",
+      title,
+      description,
+      images: {
+        url:
+          `${baseUrl}/${thumbnail}` ||
+          `${baseUrl}/icons/android-chrome-512x512.png`,
+        alt: "Post Image",
+      },
+    },
+  };
+}
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <section>{children}</section>;
+}
