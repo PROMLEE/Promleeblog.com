594008d89133ca3c830afa62038a80e872ef9cc6
PROMLEE <donghoon099@naver.com>
2024-06-14 15:54:42 +0900

    Update schema.prisma to add 'lock' field and 'url' fields to Category, Series, and Subject models

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -2,14 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const res = await prisma.post.update({
-    where: {
-      id: 1,
-    },
-    data: {
-      content: "포스트 내용 수정~",
-    },
-  });
-  return Response.json("ok!");
-}
+// export async function GET() {
+//   const res = await prisma.post.update({
+//     where: {
+//       id: 1,
+//     },
+//     data: {
+//       content: "포스트 내용 수정~",
+//     },
+//   });
+//   return Response.json("ok!");
+// }
