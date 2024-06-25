ed340ee27190e298f5f87f36dccede6d0f79e454
PROMLEE <donghoon099@naver.com>
2024-06-25 23:47:30 +0900

    chore: Update Exprience and Projects components with thumbnail images

--- src/components/aboutme/Projects.tsx
+++ src/components/aboutme/Projects.tsx
@@ -3,9 +3,11 @@ import Image from "next/image";
 interface ProjectType {
   name: string;
   url: string;
+  thumbnail?: { url: string; width: number; height: number };
   date: string;
   contents: string[];
   myrole: string[];
+  mystack: string[];
 }
 
 const projects: ProjectType[] = [
@@ -13,6 +15,11 @@ const projects: ProjectType[] = [
     name: "Wear_os Watchface",
     url: "https://github.com/PROMLEE/Wear_Os_WatchFace",
     date: "2022.05 ~ 2022.12",
+    thumbnail: {
+      url: "https://cdn.promleeblog.com/posts/etc/watchface/watchface-logo.png",
+      width: 100,
+      height: 100,
+    },
     contents: [
       "Wear Os 기반의 안드로이드 웨어러블 기기의 배경화면 커스텀 제작",
     ],
@@ -21,10 +28,16 @@ const projects: ProjectType[] = [
       "androidx.wear.watchface 라이브러리 사용",
       "원하는 배경, 원하는 시계바늘, 워하는 기능 지원하는 컴플리케이션 8개 지원",
     ],
+    mystack: ["Kotlin", "Android Studio", "Wear Os"],
   },
   {
     name: "Indoor Map",
     url: "https://github.com/PROMLEE/Indoor_map",
+    thumbnail: {
+      url: "https://github.com/PROMLEE/Indoor_map/raw/main/Indoor_map_flutter/src/ic_launcher.png",
+      width: 100,
+      height: 100,
+    },
     date: "2023.09.01 ~ 2023.12.15",
     contents: [
       "도면(안내도) 기반 image segmentation 기술을 이용한 실내 길 찾기 지원 플랫폼 제작 Team Project",
@@ -34,10 +47,16 @@ const projects: ProjectType[] = [
       "Flask API 제작 및 배포",
       "React 기반 부가기능 웹페이지 제작",
     ],
+    mystack: ["Flutter", "Flask", "React", "OpenCV", "DeepLabV3+"],
   },
   {
     name: "Map: 2 Zero",
     url: "https://github.com/PROMLEE/map2zero-frontend",
+    thumbnail: {
+      url: "https://github.com/PROMLEE/map2zero-frontend/raw/main/image/logo.png",
+      width: 100,
+      height: 100,
+    },
     date: "2024.01.05 ~ 2024.02.25",
     contents: ["지속가능한 삶을 위한 ESG 서비스 Map:Zero(맵투제로)"],
     myrole: [
@@ -45,6 +64,24 @@ const projects: ProjectType[] = [
       "소셜 로그인 구현",
       "점주의 매장 제품 등록 및 관리 폼 제작",
     ],
+    mystack: ["React", "Naver Map API"],
+  },
+  {
+    name: "이슈역",
+    url: "https://github.com/PROMLEE/Issue-Station",
+    thumbnail: {
+      url: "https://cdn.promleeblog.com/posts/etc/issue-station/issue-station-logo.png",
+      width: 100,
+      height: 30,
+    },
+    date: "2024.05.01 ~ 2024.06.10",
+    contents: ["프로젝트 단위 이슈 관리 프로그램"],
+    myrole: [
+      "React 기반 프론트엔드 웹페이지 제작",
+      "Spring Boot 기반 서버 API 제작(Project Management Part)",
+      "AWS EC2 서버 배포 및 관리",
+    ],
+    mystack: ["React", "Spring Boot", "MySQL", "AWS EC2", "AWS RDS"],
   },
 ];
 
@@ -63,7 +100,7 @@ export const Projects = () => {
                   href={project.url}
                   target="_blank"
                   rel="noreferrer"
-                  className="flex items-center gap-1"
+                  className="flex w-60 items-center gap-1 hover:text-blue-400"
                 >
                   <div className="relative h-5 w-5">
                     <Image
@@ -73,13 +110,22 @@ export const Projects = () => {
                       className="m-0"
                     />
                   </div>
-                  {project.name}
+                  {project.name} ↗
                 </a>
                 <p>{project.date}</p>
+                {project.thumbnail && (
+                  <Image
+                    src={project.thumbnail.url}
+                    alt={project.name}
+                    width={project.thumbnail.width}
+                    height={project.thumbnail.height}
+                    className="m-0 rounded-md"
+                  />
+                )}
               </div>
               <div className="w-2/3">
                 <ul className="m-0">
-                  <li>
+                  <li className="text-text-foreground marker:text-white">
                     Contents
                     <ul>
                       {project.contents.map((content, index) => {
@@ -87,11 +133,19 @@ export const Projects = () => {
                       })}
                     </ul>
                   </li>
-                  <li>
+                  <li className="text-text-foreground marker:text-white">
                     My Role
                     <ul>
                       {project.myrole.map((role, index) => {
                         return <li key={index}>{role}</li>;
+                      })}
+                    </ul>
+                  </li>
+                  <li className="text-text-foreground marker:text-white">
+                    My Stack
+                    <ul>
+                      {project.mystack.map((stack, index) => {
+                        return <li key={index}>{stack}</li>;
                       })}
                     </ul>
                   </li>
