147f48b1510537cd43571e559ea6efc719c33082
PROMLEE <donghoon099@naver.com>
2024-07-11 19:41:38 +0900

    Refactor Projects component to update @radix-ui/react-dialog to version 1.1.1

--- src/app/blog/edit/page.tsx
+++ src/app/blog/edit/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
-import { ScrollArea } from "@/components/ui/scroll-area";
 interface Link {
   nameko: string;
   url: string;
@@ -42,16 +39,16 @@ const page = () => {
     });
   }, []);
   const blockstyle =
-    "flex flex-1 flex-col gap-2 border p-2 border-text h-64 overflow-hidden select-none";
+    "flex flex-1 flex-col gap-2 border p-2 border-text min-h-64 overflow-hidden select-none";
   const titlestyle =
     "rounded-md bg-slate-400 p-2 text-center font-bold text-primary mb-3";
   return (
-    <div className="flex min-h-64 w-full rounded-lg bg-primary p-10">
-      <ScrollArea className={blockstyle}>
+    <div className="flex min-h-64 w-full rounded-lg p-10">
+      <div className={blockstyle}>
         <h2 className={titlestyle}>
           카테고리
           <button
-            className="ml-5 rounded bg-primary-foreground p-1"
+            className="ml-5 rounded bg-primary-foreground px-1"
             onClick={() => {}}
           >
             +
@@ -65,8 +62,8 @@ const page = () => {
             <p onClick={() => setCategory(item.url)}>{item.nameko}</p>
           </div>
         ))}
-      </ScrollArea>
-      <ScrollArea className={blockstyle}>
+      </div>
+      <div className={blockstyle}>
         <h2 className={titlestyle}>주제</h2>
         {Links.find((link) => link.url === category)?.Subject.map((item) => (
           <div
@@ -76,8 +73,8 @@ const page = () => {
             <p onClick={() => setSubject(item.url)}>{item.nameko}</p>
           </div>
         ))}
-      </ScrollArea>
-      <ScrollArea className={blockstyle}>
+      </div>
+      <div className={blockstyle}>
         <h2 className={titlestyle}>시리즈</h2>
         {Links.find((link) => link.url === category)
           ?.Subject.find((link) => link.url === subject)
@@ -89,7 +86,7 @@ const page = () => {
               <p onClick={() => setSeries(item.id.toString())}>{item.nameko}</p>
             </div>
           ))}
-      </ScrollArea>
+      </div>
       <div className={blockstyle}>
         <h2 className={titlestyle}>포스트</h2>
         {Links.find((link) => link.url === category)
@@ -98,68 +95,17 @@ const page = () => {
           ?.Post.map((item) => (
             <div
               key={item.id}
-              className={`${item.url === post && "bg-third dark:bg-slate-600"} block overflow-hidden text-ellipsis whitespace-nowrap rounded-md p-2`}
-              onClick={() => setPost(item.url)}
+              className={`${item.url === post && "bg-third dark:bg-slate-600"} rounded-md p-2`}
             >
-              {item.nameko}
+              <p
+                onClick={() => setPost(item.url)}
+                className="block overflow-hidden text-ellipsis whitespace-nowrap"
+              >
+                {item.nameko}
+              </p>
             </div>
           ))}
       </div>
-      {/* <Select onValueChange={(value) => setCategory(value)}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="카테고리" />
-        </SelectTrigger>
-        <SelectContent>
-          {Links.map((category) => (
-            <SelectItem key={category.url} value={category.url}>
-              {category.nameko}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      <Select onValueChange={(value) => setSubject(value)}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="주제" />
-        </SelectTrigger>
-        <SelectContent>
-          {Links.find(
-            (link_category) => link_category.url === category,
-          )?.Subject.map((subject) => (
-            <SelectItem key={subject.url} value={subject.url}>
-              {subject.nameko}
-            </SelectItem>
-          ))}
-        </SelectContent>
-      </Select>
-      <Select onValueChange={(value) => setSeries(value)}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="시리즈" />
-        </SelectTrigger>
-        <SelectContent>
-          {Links.find((link_category) => link_category.url === category)
-            ?.Subject.find((link_subject) => link_subject.url === subject)
-            ?.Series.map((series) => (
-              <SelectItem key={series.id} value={series.id.toString()}>
-                {series.nameko}
-              </SelectItem>
-            ))}
-        </SelectContent>
-      </Select>
-      <Select onValueChange={(value) => setPost(value)}>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="포스트" />
-        </SelectTrigger>
-        <SelectContent>
-          {Links.find((link_category) => link_category.url === category)
-            ?.Subject.find((link_subject) => link_subject.url === subject)
-            ?.Series.find((link_series) => link_series.id.toString() === series)
-            ?.Post.map((post) => (
-              <SelectItem key={post.id} value={post.url}>
-                {post.nameko}
-              </SelectItem>
-            ))}
-        </SelectContent>
-      </Select> */}
     </div>
   );
 };
