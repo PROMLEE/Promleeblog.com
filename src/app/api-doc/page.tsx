37ab18df515cd6ada17165744949aea6e95ef077
PROMLEE <donghoon099@naver.com>
2025-04-02 23:13:13 +0900

    fix: remove swagger-ui-react dependency and update API documentation page to return an empty div

--- src/app/api-doc/page.tsx
+++ src/app/api-doc/page.tsx
@@ -3,5 +3,6 @@ import ReactSwagger from "../../components/react-swagger";
 
 export default async function IndexPage() {
   const spec = await getApiDocs();
-  return <ReactSwagger spec={spec} url="/swagger.json" />;
+  // return <ReactSwagger spec={spec} url="/swagger.json" />;
+  return <div></div>;
 }
