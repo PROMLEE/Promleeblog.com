e9eda762a6a4927939c09d98af56cd1909a1874a
PROMLEE <donghoon099@naver.com>
2025-04-03 11:58:22 +0900

    fix: adjust positioning of header image in MdxHeader component

--- src/components/posts/MdxHeader.tsx
+++ src/components/posts/MdxHeader.tsx
@@ -10,7 +10,7 @@ export const MdxHeader = ({ props }: { props: HeaderProps }) => {
   return (
     <div className="my-28">
       <h1 className="relative z-10 mt-36 w-full text-center text-4xl font-bold text-text-foreground">
-        <div className="absolute -top-20 flex w-full flex-col items-center md:max-h-20">
+        <div className="absolute -top-10 flex w-full flex-col items-center md:max-h-20">
           <img
             className="z-[-1] h-40 opacity-20 md:h-60"
             src={
